ca74da0b66adb307d44174a83844b49b02184792
SangamChaurasiya <sangam.chaurasiya0604@gmail.com>
2024-09-23 20:30:48 +0530

    Vendor profile with google geocoding feature

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'vendor.context_processors.get_vendor',
+                'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -153,3 +154,5 @@ EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "foodOnline Marketplace <sangamdemo630@gmail.com>"
+
+GOOGLE_MAPS_API_KEY = config("GOOGLE_MAPS_API_KEY")

4a238ad31e97222b46c2749b52a120eaf3313d52
SangamChaurasiya <sangam.chaurasiya0604@gmail.com>
2024-09-24 19:55:06 +0530

    Menu builder category CRUD

--- vendor/urls.py
+++ vendor/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('registerVendor/', VendorViews.registerVendor, name="registerVendor"),
     path('vendorDashboard/', VendorViews.vendorDashboard, name="vendorDashboard"),
     path('profile/', VendorViews.vprofile, name="vprofile"),
+    path('menu-builder/', VendorViews.menu_builder, name="menu_builder"),
+    path('menu-builder/category/<int:pk>/', VendorViews.fooditems_by_category, name="fooditems_by_category"),
+
+    # Category CRUD
+    path('menu-builder/category/add/', VendorViews.add_category, name="add_category"),
+    path('menu-builder/category/edit/<int:pk>/', VendorViews.edit_category, name="edit_category"),
+    path('menu-builder/category/delete/<int:pk>/', VendorViews.delete_category, name="delete_category"),
+
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from accounts.utils import sendEmail
 from vendor.models import Vendor
+from menu.models import Category, FoodItem
+from menu.forms import CategoryForm
+from django.template.defaultfilters import slugify
 
 
 # Restricting the Vendor from accessing the Customer page
@@ -66,13 +69,13 @@ def registerVendor(request):
         'v_form': v_form
     }
 
-    return render(request, 'vendors/registerVendor.html', context)
+    return render(request, 'vendor/registerVendor.html', context)
 
 
 @login_required(login_url="accounts:login")
 @user_passes_test(checkRoleVendor)
 def vendorDashboard(request):
-    return render(request, 'vendors/vendorDashboard.html')
+    return render(request, 'vendor/vendorDashboard.html')
 
 
 @login_required(login_url='accounts:login')
@@ -103,4 +106,91 @@ def vprofile(request):
         'profile': profile,
         'vendor': vendor
     }
-    return render(request, 'vendors/vprofile.html', context)
+    return render(request, 'vendor/vprofile.html', context)
+
+
+def get_vendor(request):
+    vendor = Vendor.objects.get(user=request.user)
+    return vendor
+
+
+@login_required(login_url="accounts:login")
+@user_passes_test(checkRoleVendor)
+def menu_builder(request):
+    vendor = get_vendor(request)
+    categories = Category.objects.filter(vendor=vendor).order_by("created_at")
+    context = {
+        'categories': categories
+    }
+    return render(request, 'vendor/menu_builder.html', context=context)
+
+
+@login_required(login_url="accounts:login")
+@user_passes_test(checkRoleVendor)
+def fooditems_by_category(request, pk=None):
+    vendor = get_vendor(request)
+    category = get_object_or_404(Category, pk=pk)
+    fooditems = FoodItem.objects.filter(vendor=vendor, category=category)
+    context = {
+        'fooditems': fooditems,
+        'category': category
+    }
+    return render(request, 'vendor/fooditems_by_category.html', context)
+
+
+@login_required(login_url="accounts:login")
+@user_passes_test(checkRoleVendor)
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category_name = form.cleaned_data['category_name']
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request)
+            category.slug = slugify(category_name)
+            form.save()
+            messages.success(request, "Category added successfully!")
+            return redirect('vendor:menu_builder')
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'vendor/add_category.html', context)
+
+
+@login_required(login_url="accounts:login")
+@user_passes_test(checkRoleVendor)
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == "POST":
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            category_name = form.cleaned_data['category_name']
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request)
+            category.slug = slugify(category_name)
+            form.save()
+            messages.success(request, "Category added successfully!")
+            return redirect('vendor:menu_builder')
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm(instance=category)
+    context = {
+        'form': form,
+        'category': category
+    }
+    return render(request, 'vendor/edit_category.html', context)
+
+
+@login_required(login_url="accounts:login")
+@user_passes_test(checkRoleVendor)
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    messages.success(request, "Category has been deleted successfully.")
+    return redirect('vendor:menu_builder')
+
